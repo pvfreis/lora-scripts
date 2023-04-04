9dde46b72eb62ba0c249e6ab5530fcdf7500930a
Paulo Vitor <pvfreis@gmail.com>
2023-04-04 18:48:07 -0300

    tests

--- train.ps1
+++ train.ps1
@@ -1,7 +1,7 @@
 # LoRA train script by @Akegarasu
 
 # Train data path | 设置训练用模型、图片
-$pretrained_model = "./sd-models/v1-5-pruned.safetensor" # base model path | 底模路径
+$pretrained_model = "./sd-models/v1-5-pruned.safetensors" # base model path | 底模路径
 $train_data_dir = "./train/lrao" # train dataset path | 训练数据集路径
 $reg_data_dir = "" # directory for regularization images | 正则化数据集路径，默认不使用正则化图像。
 
@@ -14,7 +14,7 @@ $network_alpha = 32 # network alpha | 常用与 network_dim 相同的值或者�
 # Train related params | 训练相关参数
 $resolution = "512,512" # image resolution w,h. 图片分辨率，宽,高。支持非正方形，但必须是 64 倍数。
 $batch_size = 1 # batch size
-$max_train_epoches = 10 # max train epoches | 最大训练 epoch
+$max_train_epoches = 2 # max train epoches | 最大训练 epoch
 $save_every_n_epochs = 2 # save every n epochs | 每 N 个 epoch 保存一次
 
 $train_unet_only = 0 # train U-Net only | 仅训练 U-Net，开启这个会牺牲效果大幅减少显存使用。6G显存可以开启
@@ -32,7 +32,7 @@ $lr_warmup_steps = 0 # warmup steps | 仅在 lr_scheduler 为 constant_with_warm
 $lr_restart_cycles = 1 # cosine_with_restarts restart cycles | 余弦退火重启次数，仅在 lr_scheduler 为 cosine_with_restarts 时起效。
 
 # Output settings | 输出设置
-$output_name = "aki" # output model name | 模型保存名称
+$output_name = "lrao2" # output model name | 模型保存名称
 $save_model_as = "safetensors" # model save ext | 模型保存格式 ckpt, pt, safetensors
 
 # 其他设置
