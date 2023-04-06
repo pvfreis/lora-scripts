465a3e607e4824c7ca4c5adf618b4e82b066a05d
Paulo Vitor <pvfreis@gmail.com>
2023-04-06 20:00:35 -0300

    Install wheel to avoid install from setup.py

--- install.bash
+++ install.bash
@@ -2,6 +2,8 @@ echo "Creating python venv..."
 python3 -m venv venv
 source venv/bin/activate
 
+pip install wheel
+
 echo "Installing torch & xformers..."
 
 pip install torch==2.0.0+cu118 torchvision==0.15.1+cu118 --extra-index-url https://download.pytorch.org/whl/cu118
