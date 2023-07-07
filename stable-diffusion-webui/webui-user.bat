4cfe73449678c8a2247140cef21d4d5cd6180686
Paulo Vitor <pvfreis@gmail.com>
2023-07-07 15:53:48 -0300

    Disabled safe unpickle

--- stable-diffusion-webui/webui-user.bat
+++ stable-diffusion-webui/webui-user.bat
@@ -3,7 +3,7 @@
 set PYTHON=
 set GIT=
 set VENV_DIR=
-set COMMANDLINE_ARGS= --xformers --medvram --precision full --no-half --no-half-vae --opt-split-attention
+set COMMANDLINE_ARGS= --xformers --medvram --precision full --no-half --no-half-vae --opt-split-attention --disable-safe-unpickle
 set OPTIMIZED_TURBO=true
 
 call webui.bat
